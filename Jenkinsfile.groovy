b3476f9f2d289d859d1d8b1e05f19b0c2d92375f
hqzhang <zhanghongqi@hotmail.com>
2024-05-12 10:17:37 -0400

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,27 +3,34 @@
 def consul = new org.foo.Consul()
 pipeline {
     agent any
-    stages {
-       
-    try {
+    environment{
         String jsonRegisterData = "{ .. }" // A string whatever data we want in json format
         String path = "http://myconsul.com:8500/v1"
+    }
+    stages {
         stage("register data") {
-            consul.register(path, jsonRegisterData)
+            steps {
+                script {
+                     consul.register(path, jsonRegisterData)
+                }
+            }
         }
 
-        stage("store some data") {
+        stage("store some data") { 
+            steps {
+                script {
             consul.store(path, "v1/kv/test/1/data", "value1")
             consul.store(path, "v1/kv/test/2/data", "value2")
+          }
+            }
         }
         stage("list Nodes"){
+             steps {
+                script {
             consul.list_nodes(path)
+         }
+            }
         }
-        
-    }
-    catch(err){
-        //.. handle
-    }
         
     }
 }
